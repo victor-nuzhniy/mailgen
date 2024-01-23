6ee96ef71d5b6d42b7ab5a7f2e6ff5894ee67824
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-23 23:35:08 +0200

    dev added constants

--- mailgen/common/constants.py
+++ mailgen/common/constants.py
@@ -1,1 +1,11 @@
 """Constants for mailgen app."""
+
+SYMBOLS: str = '!@#$%^&*()_+'
+MONTHS_LETTERS: str = 'JFMASOND'
+GOOGLE_DOMAIN: str = 'https://google.com'
+PROTON_URL: str = 'https://account.proton.me/signup?plan=free'
+DROPMAIL_URL: str = 'https://dropmail.me/'
+COPY_IMAGE: str = 'mailgen/images/copy_icon.png'
+CAPTCHA_PLACE_IMAGE: str = 'mailgen/images/captcha_place.png'
+CAPTCHA_SAMPLE_IMAGE: str = 'mailgen/images/captcha_sample.png'
+LOGFILE: str = 'accLog.txt'
